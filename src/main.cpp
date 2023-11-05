aa171af41d4e14fca83aeaef7636d4989de6e0ca
Pedro henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2023-11-05 20:35:34 -0300

    Feat: add a few libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,59 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include <Adafruit_INA219.h>
+
 #include "models/ams5600/ams5600.h"
 #include "models/leds/leds.h"
 #include "models/rele/rele.h"
 #include "models/button/button.h"
+#include "models/servo-driver/servo_driver.h"
+#include "models/ina219/ina219.h"
 
 #define SDA_AMS 15
 #define SCL_AMS 16
 
+#define SDA_INA 42
+#define SCL_INA 2
+
+
 void setup() {
     Serial.begin(115200);
-    pinMode(4, INPUT);
-    pinMode(2, INPUT);
-    pinMode(18, INPUT);
 
-    // -------------------------------------------
-    // AMS5600 SETUP
-    // -------------------------------------------
+
+    // // -------------------------------------------
+    // // AMS5600 SETUP
+    // // -------------------------------------------
     // ams5600_begin(SDA_AMS, SCL_AMS);
     // check_magnet_presence();
     // read_raw_angle();
     // startAngle = degAngle;
 
-    // -------------------------------------------
-    // LED'S SETUP
-    // -------------------------------------------
-    if (!leds_setup()) Serial.println("Failed to setup led's.");
+    // // -------------------------------------------
+    // // LED'S SETUP
+    // // -------------------------------------------
+    // if (!leds_setup()) Serial.println("Failed to setup led's.");
 
 
-    // -------------------------------------------
-    // RELE SETUP
-    // -------------------------------------------
-    if(!rele_setup()) Serial.println("Failed to setup the rele.");
+    // // -------------------------------------------
+    // // RELE SETUP
+    // // -------------------------------------------
+    // if(!rele_setup()) Serial.println("Failed to setup the rele.");
+
+    // // -------------------------------------------
+    // // BUTTON SETUP
+    // // -------------------------------------------
+    // if (!button_setup()) Serial.println("Failed to setup button.");
+    
+    // // -------------------------------------------
+    // // SERVO DRIVER SETUP
+    // // -------------------------------------------
+    // if (!servo_driver_setup()) Serial.println("Failed to setup servo driver.");
 
     // -------------------------------------------
-    // BUTTON SETUP
+    // INA219 SETUP
     // -------------------------------------------
-    if (!button_setup()) Serial.println("Failed to setup button.");
+    if(!ina219_setup(SDA_INA, SCL_INA)) Serial.println("Failed to setup ina219.");
 }
 
 void loop() {
@@ -66,6 +82,29 @@ void loop() {
     // // -------------------------------------------
     // // BUTTON USAGE
     // // -------------------------------------------
-    bool state = get_button_state();
-    Serial.println(state);
+    // bool state = get_button_state();
+    // Serial.println(state);
+
+    // // -------------------------------------------
+    // // OPTO USAGE
+    // // -------------------------------------------
+    // servo_driver(50, 10);
+
+    // -------------------------------------------
+    // INA219 USAGE
+    // -------------------------------------------
+    get_shunt_voltage_mV();
+    get_bus_voltage_V();
+    get_current_mA();
+    get_power_mW();
+    get_load_voltage_V();
+    
+    Serial.print("Bus Voltage:   "); Serial.print(bus_voltage); Serial.println(" V");
+    Serial.print("Shunt Voltage: "); Serial.print(shunt_voltage); Serial.println(" mV");
+    Serial.print("Load Voltage:  "); Serial.print(load_voltage); Serial.println(" V");
+    Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+    Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+    Serial.println("");
+
+    delay(2000);
 }
